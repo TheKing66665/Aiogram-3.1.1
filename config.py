33d204ca4179069a8616794b41eb4dd7905b2092
TheKing66665 <162513293+TheKing66665@users.noreply.github.com>
2024-08-17 20:38:04 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,25 +1,25 @@
-BOT_TOKEN = '6518868749:AAGC1Fip7_N6-0IGsAnmy-Vjh5iOqqctqSw'
-ADMIN_ID = '6411916585'
+BOT_TOKEN = ''
+ADMIN_ID = ''
 
 
 
 # AAIO Settings
-API_KEY = 'eee3098f-a3eb-446c-b1c2-b00d7df45835'  # Используем ваш Merchant ID как API Key
-SECRET_KEY = 'd8eafdc132b93249588aa4e69259ae53'  # Секретный ключ №1
+API_KEY = ''  # Используем ваш Merchant ID как API Key
+SECRET_KEY = ''  # Секретный ключ №1
 
 
 # Crypto Bot Settings
-CRYPTO_TOKEN = '132003:AAZ53biv02KEbuoQuek7b3u7rSAExftJSMS'
+CRYPTO_TOKEN = ''
 
 
 
 
 # LavaPay Settings
-LAVA_SHOP_ID = 'ef43f019-c15f-44a5-a09e-651e9d44cf46'  # Замените на ваш идентификатор магазина LavaPay
-LAVA_SECRET_KEY = 'a625418ce4309df0e9af4d14eb9d3dbb4d21b8ae'
+LAVA_SHOP_ID = ''  # Замените на ваш идентификатор магазина LavaPay
+LAVA_SECRET_KEY = ''
 
 
 
 #CrystalPay Settings
-CRYSTALPAY_LOGIN = 'thekin666'
-CRYSTALPAY_SECRET = '8c794fcfc0e46139446236de9d25fc1bfa77b3ff'
+CRYSTALPAY_LOGIN = ''
+CRYSTALPAY_SECRET = ''
